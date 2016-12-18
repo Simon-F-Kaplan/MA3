913e5bcf6ed55b49d6886109facc33aedabdd034
Simon Ferris <simon_uk71@hotmail.com>
2016-12-18 23:03:32 +0000

    First pass at AddABookDTO

---
+++ MessageAdapter/MessageAdapter/AddABookDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageAdapter
+{
+    public class AddABookDTO
+    {
+        public Guid MessageId { get; set; }
+        public Guid UserId { get; set; }
+        public List<Resource> Resources { get; set; }
+        public DateTime MessageCreated { get; set; }
+        public string MessageType { get; set; }
+        public string Creator { get; set; }
+        public Guid CorellationId { get; set; }
+
+    }
+
+    public class Resource
+    {
+        public string Type { get; set; }
+    }
+}
